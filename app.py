17cea087d65ff364f4780e8ad69bbadd427b13f6
hanimhanna <68084815+hanimhanna@users.noreply.github.com>
2025-07-13 01:33:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def track_image():
         return abort(404, "Image not found.")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
